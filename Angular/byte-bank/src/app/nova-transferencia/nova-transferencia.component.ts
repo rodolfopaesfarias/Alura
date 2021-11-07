55a576940809612ab9f20b21c24f946a26e570c6
Rodolfo Paes de Farias <rodolfo@movtech.inf.br>
2021-11-07 14:59:19 -0300

    Segunda aula

--- Angular/byte-bank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ Angular/byte-bank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,10 +1,15 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'nova-transferencia',
-    templateUrl: './nova-transferencia.component.html',
-    styleUrls: ['./nova-transferencia.component.scss']
+  selector: 'nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
+  valor!: number;
+  destino!: number;
 
+  transferir() {
+    console.log('Solicitada transferência', this.valor, this.destino);
+  }
 }
