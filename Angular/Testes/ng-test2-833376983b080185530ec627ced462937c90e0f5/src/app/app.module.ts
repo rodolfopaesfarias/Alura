71681e265a5c863a802d2dbb651a9740fb85d855
Rodolfo Paes de Farias <rodolfo@movtech.com.br>
2022-03-22 12:56:12 -0300

    Aula 4

--- Angular/Testes/ng-test2-833376983b080185530ec627ced462937c90e0f5/src/app/app.module.ts
+++ Angular/Testes/ng-test2-833376983b080185530ec627ced462937c90e0f5/src/app/app.module.ts
@@ -1,6 +1,8 @@
+import { PhotoBoardModule } from './shared/components/photo-board/photo-board.module';
 import { PhotoFrameModule } from './shared/components/photo-frame/photo-frame.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +16,9 @@ import { LikeWidgetModule } from './shared/components/like-widget/like-widget.mo
     BrowserModule,
     AppRoutingModule,
     LikeWidgetModule,
-    PhotoFrameModule
+    PhotoFrameModule,
+    HttpClientModule,
+    PhotoBoardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Angular/Testes/ng-test2-833376983b080185530ec627ced462937c90e0f5/src/app/shared/components/photo-board/interfaces/photo.ts
@@ -0,0 +1,5 @@
+export interface Photo {
+  id: number;
+  url: String;
+  description: string;
+}
